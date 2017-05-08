2e9eedfc8d10574eae499bb878e4bfb77ff688e4
Val Okafor <okaatech@yahoo.com>
2017-05-07 23:17:59 -0700

    Add select attachment dialog

--- app/src/main/java/com/okason/diary/ui/addnote/AddNoteActivity.java
+++ app/src/main/java/com/okason/diary/ui/addnote/AddNoteActivity.java
@@ -1,12 +1,23 @@
 package com.okason.diary.ui.addnote;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.mikepenz.materialdrawer.Drawer;
 import com.okason.diary.R;
 import com.okason.diary.utils.Constants;
 
@@ -14,8 +25,12 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AddNoteActivity extends AppCompatActivity {
+    private Drawer result = null;
 
     private final static String LOG_TAG = AddNoteActivity.class.getSimpleName();
+    private Activity mActivity;
+    @BindView(android.R.id.content)
+    View mRootView;
 
 
     @BindView(R.id.toolbar) Toolbar toolbar;
@@ -26,7 +41,9 @@ public class AddNoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_note);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+        mActivity = this;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_check_white_24dp);
 
         if (savedInstanceState == null) {
             if (getIntent() != null && getIntent().hasExtra(Constants.NOTE_ID)){
@@ -36,7 +53,16 @@ public class AddNoteActivity extends AppCompatActivity {
                 openFragment(NoteEditorFragment.newInstance(""), getString(R.string.add_new_journal));
             }
         }
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_note, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 
     private void openFragment(Fragment fragment, String screenTitle){
         getSupportFragmentManager()
@@ -60,9 +86,11 @@ public class AddNoteActivity extends AppCompatActivity {
                     getFragmentManager().popBackStack();
                 }
                 return true;
-            default:
-                return super.onOptionsItemSelected(item);
+            case R.id.action_attachment:
+                showSelectAttachmentDialog();
+                break;
         }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -73,6 +101,42 @@ public class AddNoteActivity extends AppCompatActivity {
         } else {
             getFragmentManager().popBackStack();
         }
+    }
+
+    private void showSelectAttachmentDialog() {
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mActivity);
+        LayoutInflater inflater = mActivity.getLayoutInflater();
+
+        final View layout = (View) inflater.inflate(R.layout.attachment_dialog, null);
+        alertDialog.setView(layout);
+
+        View titleView = (View)inflater.inflate(R.layout.dialog_title, null);
+        TextView titleText = (TextView)titleView.findViewById(R.id.text_view_dialog_title);
+        titleText.setText("Select Attachment");
+        alertDialog.setCustomTitle(titleView);
+        final Dialog dialog = alertDialog.create();
+        dialog.show();
+
+        TextView cameraSelection = (TextView) layout.findViewById(R.id.camera);
+        cameraSelection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeToast("Camera clicked");
+            }
+        });
+
+
+
+    }
+
+    private void makeToast(String message){
+        Snackbar snackbar = Snackbar.make(mRootView, message, Snackbar.LENGTH_LONG);
+        View snackBarView = snackbar.getView();
+        snackBarView.setBackgroundColor(ContextCompat.getColor(mActivity, R.color.primary));
+        TextView tv = (TextView)snackBarView.findViewById(android.support.design.R.id.snackbar_text);
+        tv.setTextColor(Color.WHITE);
+        snackbar.show();
     }
 
 }
--- app/src/main/java/com/okason/diary/ui/addnote/NoteEditorFragment.java
+++ app/src/main/java/com/okason/diary/ui/addnote/NoteEditorFragment.java
@@ -1,15 +1,20 @@
 package com.okason.diary.ui.addnote;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.okason.diary.R;
 import com.okason.diary.utils.Constants;
@@ -22,7 +27,7 @@ public class NoteEditorFragment extends Fragment {
     private final static String LOG_TAG = "NoteEditorFragment";
 
     private View mRootView;
-    private boolean showLinedEditor = false;
+
 
 
     public NoteEditorFragment() {
@@ -32,13 +37,9 @@ public class NoteEditorFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-        showLinedEditor = PreferenceManager
-                .getDefaultSharedPreferences(getContext()).getBoolean("default_editor", false);
-        Log.d(LOG_TAG, "Line Editor Enabled ?: " + showLinedEditor);
+
+
     }
-
-
 
     public static NoteEditorFragment newInstance(String noteId){
         NoteEditorFragment fragment = new NoteEditorFragment();
@@ -58,8 +59,38 @@ public class NoteEditorFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mRootView = inflater.inflate(R.layout.fragment_note_editor, container, false);
-
         return mRootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_add_note, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        int id = item.getItemId();
+        switch (id){
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+    private void makeToast(String message){
+        Snackbar snackbar = Snackbar.make(mRootView, message, Snackbar.LENGTH_LONG);
+        View snackBarView = snackbar.getView();
+        snackBarView.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.primary));
+        TextView tv = (TextView)snackBarView.findViewById(android.support.design.R.id.snackbar_text);
+        tv.setTextColor(Color.WHITE);
+        snackbar.show();
     }
 
 }
